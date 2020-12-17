4a813ffb38175644f79cbe84170b68eca523e3c4
ASHOK-PALA <aspfreelance@gmail.com>
2020-12-17 18:03:25 +0530

    version5

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(10).fadeOut('slow', function() {
         $(this).remove();
       });
     }
