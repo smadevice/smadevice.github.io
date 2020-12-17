c7e4718cc19e1bbc4de11f5b86526ecc96894c5f
ASHOK-PALA <aspfreelance@gmail.com>
2020-12-17 18:25:28 +0530

    version6

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,40 +145,5 @@
     return false;
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      900: {
-        items: 2
-      }
-    }
-  });
-
-  // Initiate the venobox plugin
-  $(document).ready(function() {
-    $('.venobox').venobox();
-  });
-
-  // Initiate the datepicker plugin
-  $(document).ready(function() {
-    $('.datepicker').datepicker({
-      autoclose: true
-    });
-  });
 
 })(jQuery);
